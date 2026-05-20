6b8e34df468598648e64acb3b423807c503dfe8a
bfbarkhouse-redpanda <ben.barkhouse@redpanda.com>
2026-05-20 08:53:10 -0400

    main: dispatch 'add' subcommand before TUI launch

--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/bfbarkhouse-redpanda/terraview/internal/cmd"
 	"github.com/bfbarkhouse-redpanda/terraview/internal/config"
 	"github.com/bfbarkhouse-redpanda/terraview/internal/tui"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "add" {
+		if err := cmd.RunAdd(os.Args[2:]); err != nil {
+			fmt.Fprintln(os.Stderr, "error:", err)
+			os.Exit(1)
+		}
+		return
+	}
+
 	cfg, err := config.Load()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error loading config: %v\n", err)
